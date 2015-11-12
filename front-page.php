bb99610e995c44d66b3f5b5e2f669a3a23e3f42e
Rick Butterfield <rick.butterfield@gmail.com>
2015-11-12 13:35:26 +0000

    Base font size change

--- front-page.php
+++ front-page.php
@@ -3,6 +3,20 @@
 <div class="o-container">
   <div class="o-row">
     <div class="o-col-xs-12 o-col-md-8 o-col-md--offset-2">
+
+      <p>Perry Noble often says that every number has a name, every name has a story and every story matters to God. Which is why we count numbers, and which is why we want to <em>get</em> numbers! We want to get phone numbers, email addresses and as much information about the people of Valley Church as possible. Why is that? Here are eight reasons:</p>
+      <ol>
+      <li><strong>To care for people.</strong> Without a phone number, how can we follow up on a prayer request, a difficult situation or someone who’s not able to attend due to sickness?</li>
+      <li><strong>To invite people.</strong> People don’t serve events, events serve people. And so how can we get the right people to the right things if we can’t invite them personally?</li>
+      <li><strong>To show that we’re interested.</strong> Every single human being is a unique image-bearer of God. That’s something we should be interested in! Being interested in people means committing to connect with them, and that usually requires a phone number!</li>
+      <li><strong>To provide opportunity for ownership.</strong> Becoming an integral part of a community involves being able to get in touch. The body of Christ is something people <em>need</em> to be a part of and taking the responsibility to give your details moves you from spectator to participant.</li>
+      <li><strong>To help people take their best next step.</strong> Knowing where people are at involves talking to them, getting to know them and finding out information about them. Then we can serve every unique individual by responding faithfully to the requests they have logged on a details card.</li>
+      <li><strong>To facilitate personal invitation.</strong> A general invite is never as good as a personal one. Most people start off in church life being dragged along. Let’s faithfully and patiently come alongside people on an individual basis and take the time to help them connect to real community.</li>
+      <li><strong>To give the church permission.</strong> When someone gives in their details, the church has permission to contact them in a non-weird way. We have been welcomed in to a life that God values so much, and now we have opportunity and permission to serve that life.</li>
+      <li><strong>To ensure accountability.</strong> Shepherding the flock, and knowing the flock are two things God takes seriously. Ensuring that every individual has the <em>best possible opportunity</em> to connect to the church family requires a system of accountability, hence, details cards.</li>
+      </ol>
+      <p>&#8211; Dave</p>
+
       <h1>This is the primary heading and there should only be one of these per page</h1>
       <p class="lead">This is a <strong>lead paragraph</strong>. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>
       <p>A small paragraph to <em>emphasise</em> and show <strong>important</strong> bits.</p>
@@ -63,98 +77,98 @@
       <h4>A sub heading which is not as important as the second, but should be used with consideration</h4>
       <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>
       <pre><code>#header h1 a {
-  display: block;
-  width: 300px;
-  height: 80px;
-}</code></pre>
-    <h5>A sub heading which is not as important as the second, but should be used with consideration</h5>
-    <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>
-    <dl>
-     <dt>Definition list</dt>
-     <dd>Consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
-      aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-      commodo consequat.</dd>
-      <dt>Lorem ipsum dolor sit amet</dt>
-      <dd>Consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+        display: block;
+        width: 300px;
+        height: 80px;
+      }</code></pre>
+      <h5>A sub heading which is not as important as the second, but should be used with consideration</h5>
+      <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>
+      <dl>
+       <dt>Definition list</dt>
+       <dd>Consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
         aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
         commodo consequat.</dd>
-      </dl>
-      <h6>This heading plays a relatively small bit part role, if you use it at all</h6>
-      <p><small>And this denotes small text</small></p>
-      <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>
-      <hr/>
-      <h6>Forms</h6>
-      <form>
-        <label>
-          Email address
-        </label>
-        <input type="email" placeholder="come@me.bro" pattern="^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$">
+        <dt>Lorem ipsum dolor sit amet</dt>
+        <dd>Consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+          aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+          commodo consequat.</dd>
+        </dl>
+        <h6>This heading plays a relatively small bit part role, if you use it at all</h6>
+        <p><small>And this denotes small text</small></p>
+        <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>
+        <hr/>
+        <h6>Forms</h6>
+        <form>
+          <label>
+            Email address
+          </label>
+          <input type="email" placeholder="come@me.bro" pattern="^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$">
 
-        <div class="o-row">
+          <div class="o-row">
 
-          <div class="o-col-xs-6">
-            <label>
-              Password
-            </label>
-            <input placeholder="Keep it secret." type="password">
+            <div class="o-col-xs-12 o-col-sm-6">
+              <label>
+                Password
+              </label>
+              <input placeholder="Keep it secret." type="password">
+            </div>
+
+            <div class="o-col-xs-12 o-col-sm-6">
+              <label>
+                Confirm password
+              </label>
+              <input placeholder="Keep it safe." type="password">
+            </div>
+
+            <div class="o-col-xs-12 o-col-sm-6">
+              <label>
+                Disabled field
+              </label>
+              <input type="text" value="This is disabled" disabled>
+            </div>
+
+            <div class="o-col-xs-12 o-col-sm-6">
+              <label>
+                Textarea
+              </label>
+              <textarea>Edit me, yo</textarea>
+            </div>
+
+            <div class="o-col-xs-12 o-col-sm-4">
+              <label>
+                First name <span class="u-font-error">(Required)</span>
+              </label>
+              <input type="text" class="is-invalid">
+            </div>
+
+            <div class="o-col-xs-12 o-col-sm-4">
+              <label>
+                Last name
+              </label>
+              <input type="text" class="is-valid" value="Housten">
+            </div>
+
+            <div class="o-col-xs-12 o-col-sm-4">
+              <label>
+                Country
+              </label>
+              <select>
+                <option>United Kingdom</option>
+                <option>United States</option>
+                <option>Something else</option>
+              </select>
+            </div>
+
+            <div class="o-col-sm-12">
+              <button type="submit" class="o-btn">
+                Submit
+              </button>
+            </div>
+
           </div>
-
-          <div class="o-col-xs-6">
-            <label>
-              Confirm password
-            </label>
-            <input placeholder="Keep it safe." type="password">
-          </div>
-
-          <div class="o-col-xs-6">
-            <label>
-              Disabled field
-            </label>
-            <input type="text" value="This is disabled" disabled>
-          </div>
-
-          <div class="o-col-xs-6">
-            <label>
-              Textarea
-            </label>
-            <textarea>Edit me, yo</textarea>
-          </div>
-
-          <div class="o-col-xs-4">
-            <label>
-              First name <span class="u-font-error">(Required)</span>
-            </label>
-            <input type="text" class="is-invalid">
-          </div>
-
-          <div class="o-col-xs-4">
-            <label>
-              Last name
-            </label>
-            <input type="text" class="is-valid" value="Housten">
-          </div>
-
-          <div class="o-col-xs-4">
-            <label>
-              Country
-            </label>
-            <select>
-              <option>United Kingdom</option>
-              <option>United States</option>
-              <option>Something else</option>
-            </select>
-          </div>
-
-          <div class="o-col-xs-12">
-            <button type="submit" class="o-btn">
-              Submit
-            </button>
-          </div>
-
-        </div>
-      </form>
+        </form>
+      </div>
     </div>
   </div>
-</div>
 
-<?php get_footer(); ?>
+  <?php get_footer(); ?>
--- searchform.php
+++ searchform.php
@@ -1,4 +1,10 @@
 <form role="search" method="get" id="searchform" class="c-search u-pull-right" action="<?php echo home_url( '/' ); ?>">
-  <input type="search" id="s" name="s" value="" />
-  <button type="submit" id="searchsubmit" class="o-btn">Search</button>
+  <div class="o-flag">
+    <div class="o-flag__flex">
+      <input type="search" id="s" name="s" value="" />
+    </div>
+    <div class="o-flag__fix">
+      <button type="submit" id="searchsubmit" class="o-btn">Go</button>
+    </div>
+  </div>
 </form>
