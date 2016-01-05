8a9cc331515578999d64377af57877a37994ce5f
Rick Butterfield <rick.butterfield@gmail.com>
2016-01-05 11:29:15 +0000

    Single `.c-section`

--- single.php
+++ single.php
@@ -1,79 +1,82 @@
 <?php get_header(); ?>
 
-      <?php get_template_part( 'partials/featured-image' ); ?>
+  <?php get_template_part( 'partials/featured-image' ); ?>
 
-      <article <?php post_class( 'o-container c-article u-margin' ); ?> role="article">
+  <section class="c-section">
 
-        <div class="o-row">
+    <article <?php post_class( 'o-container c-article u-margin' ); ?> role="article">
 
-          <div class="o-col-xs-12 o-col-md-7 c-post-content u-center-block">
+      <div class="o-row">
 
-            <div class="o-flag u-margin--double">
-              <div class="o-flag__fix">
-                <?php get_template_part( 'partials/avatar' ); ?>
-              </div>
-              <div class="o-flag__flex">
-                <p class="u-margin--none">
-                  <?php the_author(); ?>
-                </p>
-                <p class="small u-text-muted u-margin--none">
-                  <strong>
-                    <time datetime="<?php the_time('c'); ?>"><?php the_time('F jS, Y'); ?></time>
-                  </strong>
-                </p>
-              </div>
+        <div class="o-col-xs-12 o-col-md-7 c-post-content u-center-block">
+
+          <div class="o-flag u-margin--double">
+            <div class="o-flag__fix">
+              <?php get_template_part( 'partials/avatar' ); ?>
             </div>
+            <div class="o-flag__flex">
+              <p class="u-margin--none">
+                <?php the_author(); ?>
+              </p>
+              <p class="small u-text-muted u-margin--none">
+                <strong>
+                  <time datetime="<?php the_time('c'); ?>"><?php the_time('F jS, Y'); ?></time>
+                </strong>
+              </p>
+            </div>
+          </div>
 
-            <?php
-              $twitter = get_the_author_meta( 'twitter', $post->post_author );
-              $facebook = get_the_author_meta( 'facebook', $post->post_author );
-              $instagram = get_the_author_meta( 'instagram', $post->post_author );
-              if ( $twitter || $facebook || $instagram ) {
-            ?>
-            <p class="author-social">
-              <?php if ( $twitter ) { ?>
-              <a href="http://twitter.com/<?php echo $twitter; ?>" target="_blank">
-                <i class="fa fa-lg fa-twitter-square"></i>
-              </a>
-              <?php }
-              if ( $facebook ) { ?>
-              <a href="http://facebook.com/<?php echo $facebook; ?>" target="_blank">
-                <i class="fa fa-lg fa-facebook-square"></i>
-              </a>
-              <?php }
-              if ( $instagram ) { ?>
-              <a href="http://instagram.com/<?php echo $instagram; ?>" target="_blank">
-                <i class="fa fa-lg fa-instagram"></i>
-              </a>
-              <?php } ?>
-            </p>
+          <?php
+            $twitter = get_the_author_meta( 'twitter', $post->post_author );
+            $facebook = get_the_author_meta( 'facebook', $post->post_author );
+            $instagram = get_the_author_meta( 'instagram', $post->post_author );
+            if ( $twitter || $facebook || $instagram ) {
+          ?>
+          <p class="author-social">
+            <?php if ( $twitter ) { ?>
+            <a href="http://twitter.com/<?php echo $twitter; ?>" target="_blank">
+              <i class="fa fa-lg fa-twitter-square"></i>
+            </a>
+            <?php }
+            if ( $facebook ) { ?>
+            <a href="http://facebook.com/<?php echo $facebook; ?>" target="_blank">
+              <i class="fa fa-lg fa-facebook-square"></i>
+            </a>
+            <?php }
+            if ( $instagram ) { ?>
+            <a href="http://instagram.com/<?php echo $instagram; ?>" target="_blank">
+              <i class="fa fa-lg fa-instagram"></i>
+            </a>
             <?php } ?>
+          </p>
+          <?php } ?>
 
-            <h1><?php the_title(); ?></h1>
+          <h1><?php the_title(); ?></h1>
 
-            <?php the_content(); ?>
-
-          </div>
-
-        </div>
-
-      </article>
-
-
-      <section class="comments o-container">
-
-        <div class="o-row">
-
-          <div class="o-col-xs-12 o-col-md-7 c-post-content u-center-block">
-
-            <hr/>
-
-            <?php comments_template(); ?>
-
-          </div>
+          <?php the_content(); ?>
 
         </div>
 
-      </section>
+      </div>
+
+    </article>
+
+    </section>
+
+    <section class="comments c-section o-container">
+
+      <div class="o-row">
+
+        <div class="o-col-xs-12 o-col-md-7 c-post-content u-center-block">
+
+          <hr/>
+
+          <?php comments_template(); ?>
+
+        </div>
+
+      </div>
+
+    </section>
 
 <?php get_footer(); ?>
