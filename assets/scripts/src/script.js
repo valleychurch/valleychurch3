bb7a8ce904ede8f48b699c681066b56b10d46f3e
Rick Butterfield <rick.butterfield@gmail.com>
2017-06-15 10:42:03 +0100

    v3.2.12 - Change to font weights

--- assets/scripts/src/script.js
+++ assets/scripts/src/script.js
@@ -33,7 +33,7 @@ var Valley = (function() {
     /**
      * Scoped variables inside `Valley.#` for storing key bits of information
      */
-    Version: '3.2.11',
+    Version: '3.2.12',
 
     /**
      * Kick everything off
