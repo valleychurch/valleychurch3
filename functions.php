52f3cc2141b7e353915c3c8143773c1838063b92
Rick Butterfield <rick.butterfield@gmail.com>
2017-09-11 16:19:25 +0100

    Add ChurchSuite groups import

--- functions.php
+++ functions.php
@@ -863,7 +863,208 @@ add_action( "wp_ajax_nopriv_import_churchsuite_events", "import_churchsuite_even
 /**
  * Add ChurchSuite groups to WP
  */
+function import_churchsuite_groups() {
+  $ch = curl_init( CS_GROUPS_URL );
 
+  curl_setopt_array( $ch,
+    array(
+      CURLOPT_TIMEOUT => 0,
+      CURLOPT_RETURNTRANSFER => true,
+      CURLOPT_SSL_VERIFYPEER => false,
+      CURLOPT_HTTPHEADER => array(
+        'X-Account: ' . CS_APP_ACCOUNT,
+        'X-Application: ' . CS_APP_APPLICATION,
+        'X-Auth: ' . CS_APP_AUTH
+      )
+    )
+  );
+
+  $result = curl_exec($ch);
+
+  curl_close($ch);
+
+  $json = json_decode($result);
+  $groups = $json->groups;
+  $email = "Request URL: " . CS_GROUPS_URL . "<br/><br/>";
+
+  foreach( $groups as $group ) {
+    if ( $group->public_visible == "1" && $group->signup_enabled == "1" ) {
+      //Check against currently added groups
+      $wp_group = new WP_Query(
+        array(
+          'post_count'  => -1,
+          'post_type'   => 'connect',
+          'meta_key'    => 'identifier',
+          'meta_value'  => $group->identifier
+          )
+        );
+
+      if ( $wp_group->post_count == 0 ) {
+        $post_id = wp_insert_post(
+          array(
+            'post_type' => 'connect',
+            'post_title' => $group->name,
+            'post_content' => $group->description,
+            'post_status' => 'publish',
+            'post_author' => 2,
+            'post_date' => $group->signup_date_start,
+            'comment_status' => 'closed',
+            'ping_status' => 'closed',
+          )
+        );
+
+        // If it's successfully added to the DB
+        if ( $post_id ) {
+          $email .= "Successfully added " . $group->name . " <a href='" . get_edit_post_link( $post_id ) . "'>(ID: " . $post_id . ")</a> to WP<br/>";
+
+          // Add expiry date
+          _scheduleExpiratorEvent( $post_id, strtotime( $group->signup_date_end ), array( 'expireType' => 'draft', 'id' => $post_id ) );
+
+          // Update ACF fields
+          update_field( 'field_59b642a9ed98e', $group->identifier, $post_id );
+          update_field( 'field_59b642d0ed98f', $group->date_start, $post_id );
+          update_field( 'field_59b642dbed990', $group->date_end, $post_id );
+          update_field( 'field_59b64450ed999', $group->frequency, $post_id );
+          if ( $group->day !== null ) {
+            update_field( 'field_59b642f0ed991', $group->day, $post_id );
+          }
+          else {
+            update_field( 'field_59b642f0ed991', 7, $post_id );
+          }
+          update_field( 'field_59b6432ced992', $group->time, $post_id );
+
+          if ( count( $group->location ) != 0 ) {
+            update_field( 'field_59b64389ed994', $group->location->name, $post_id );
+            update_field( 'field_59b64390ed995', $group->location->address, $post_id );
+            update_field( 'field_59b6439aed996', $group->location->latitude, $post_id );
+            update_field( 'field_59b643a0ed997', $group->location->longitude, $post_id );
+          }
+
+          if ($group->signup_full) {
+            update_field( 'field_59b64354ed993', 1, $post_id );
+          }
+          else {
+            update_field( 'field_59b64354ed993', 0, $post_id );
+          }
+          update_field( 'field_59b643a9ed998', 'https://valley.churchsuite.co.uk/groups/' . $group->identifier, $post_id );
+        }
+
+        // Fail, do something with the error?
+        else {
+          $email .= "Couldn't add " . $group->name . " to WP<br/>";
+        }
+      }
+
+      // Run an update?
+      else {
+        $post_to_update = $wp_group->posts[0];
+        $post_id = $post_to_update->ID;
+        $update_count = 1;
+
+        //Basic object to add data to
+        $updated_info = array(
+          'ID' => $post_id
+        );
+
+        if ( esc_html($post_to_update->post_title) != esc_html( $group->name ) ) {
+          $updated_info["post_title"] = $group->name;
+          $update_count++;
+        }
+
+        if ( $post_to_update->post_content != $group->description ) {
+          $updated_info["post_content"] = $group->description;
+          $update_count++;
+        }
+
+        if ( get_field( 'identifier', $post_to_update) != $group->identifier ) {
+          update_field( 'field_59b642a9ed98e', $group->identifier, $post_id );
+          $update_count++;
+        }
+
+        if ( get_field( 'date_start', $post_to_update) != $group->date_start ) {
+          update_field( 'field_59b642d0ed98f', $group->date_start, $post_id );
+          $update_count++;
+        }
+
+        if ( get_field( 'date_end', $post_to_update) != $group->date_end ) {
+          _unscheduleExpiratorEvent( $post_id );
+          _scheduleExpiratorEvent( $post_id, strtotime( $group->datetime_end ), array( 'expireType' => 'draft', 'id' => $post_id ) );
+
+          update_field( 'field_59b642dbed990', $group->date_end, $post_id );
+          $update_count++;
+        }
+
+        if ( get_field( 'frequency', $post_to_update) != $group->frequency ) {
+          update_field( 'field_59b64450ed999', $group->frequency, $post_id );
+          $update_count++;
+        }
+
+        if ( get_field( 'day', $post_to_update) != $group->day ) {
+          if ( $group->day !== null ) {
+            update_field( 'field_59b642f0ed991', $group->day, $post_id );
+          }
+          else {
+            update_field( 'field_59b642f0ed991', 7, $post_id );
+          }
+          $update_count++;
+        }
+
+        if ( get_field( 'time', $post_to_update) != $group->time ) {
+          update_field( 'field_59b6432ced992', $group->time, $post_id );
+          $update_count++;
+        }
+
+        if ( count( $group->location ) != 0 ) {
+          if ( get_field( 'location', $post_to_update) != $group->location->name ) {
+            update_field( 'field_59b64389ed994', $group->location->name, $post_id );
+            $update_count++;
+          }
+
+          if ( get_field( 'location_address', $post_to_update) != $group->location->address ) {
+            update_field( 'field_59b64390ed995', $group->location->address, $post_id );
+            $update_count++;
+          }
+
+          if ( get_field( 'location_latitude', $post_to_update) != $group->location->latitude ) {
+            update_field( 'field_59b6439aed996', $group->location->latitude, $post_id );
+            $update_count++;
+          }
+
+          if ( get_field( 'location_longitude', $post_to_update) != $group->location->longitude ) {
+            update_field( 'field_59b643a0ed997', $group->location->longitude, $post_id );
+            $update_count++;
+          }
+        }
+
+        if ($group->signup_full) {
+          update_field( 'field_59b64354ed993', 1, $post_id );
+        }
+        else {
+          update_field( 'field_59b64354ed993', 0, $post_id );
+        }
+
+        // Update post if there's more data than just ID
+        if ( $update_count > 1 ) {
+          wp_update_post( $updated_info );
+          $email .= "Updated " . $group->name . " (ID: " . $post_id . ")<br/>";
+        }
+
+        else {
+          $email .= "No need to update " . $group->name . " <a href='" . get_edit_post_link( $post_id ) . "'>(ID: " . $post_id . ")</a><br/>";
+        }
+      }
+    }
+  }
+
+  if ( !empty( $email ) ) {
+    echo $email;
+    wp_mail( 'web@valleychurch.eu', 'ChurchSuite groups import ' . date( 'd/m/Y' ), $email, array( 'Content-Type: text/html; charset=UTF-8' ) );
+  }
+}
+
+add_action( "import_churchsuite_groups", "import_churchsuite_groups" );
+add_action( "wp_ajax_import_churchsuite_groups", "import_churchsuite_groups" );
+add_action( "wp_ajax_nopriv_import_churchsuite_groups", "import_churchsuite_groups" );
 
 /**
  * Add media to WP by URL
