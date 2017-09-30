89d81f9eb1c63274a2d3926e54cefd99ae226e4d
Rick Butterfield <rick.butterfield@gmail.com>
2017-09-30 10:50:24 +0100

    Regroup CS events by sequence

--- functions.php
+++ functions.php
@@ -9,7 +9,7 @@ define( 'CS_APP_ACCOUNT', 'valley' );
 define( 'CS_APP_APPLICATION', 'valleychurch-website' );
 define( 'CS_APP_AUTH', 'Dg8lHr5mIg30qcVdN7Je' );
 
-define( 'CS_EVENTS_URL', 'https://api.churchsuite.co.uk/v1/calendar/events?per_page=200' );
+define( 'CS_EVENTS_URL', 'https://api.churchsuite.co.uk/v1/calendar/events?per_page=200&group_by_sequence=true' );
 define( 'CS_GROUPS_URL', 'https://api.churchsuite.co.uk/v1/smallgroups/groups?view=active_future' );
 
 include( ABSPATH . 'wp-admin/includes/image.php' );
