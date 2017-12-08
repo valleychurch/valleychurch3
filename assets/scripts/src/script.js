2b06fd6799b4ecde1b45737c041b329b1ca4efbf
Rick Butterfield <rick.butterfield@gmail.com>
2017-12-08 12:00:24 +0000

    v3.4.0-alpha.1 - Add WooCommerce template files

--- assets/scripts/src/script.js
+++ assets/scripts/src/script.js
@@ -33,7 +33,7 @@ var Valley = (function() {
     /**
      * Scoped variables inside `Valley.#` for storing key bits of information
      */
-    Version: '3.4.0-alpha',
+    Version: '3.4.0-alpha.1',
 
     /**
      * Kick everything off
