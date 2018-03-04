21e223f1bdb633abff75064ee6996ae467e91aaa
Rick Butterfield <rick.butterfield@gmail.com>
2018-03-04 21:17:13 +0000

    Theme tweaks

--- woocommerce/loop/loop-start.php
+++ woocommerce/loop/loop-start.php
@@ -20,4 +20,4 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 ?>
-<div class="products columns-<?php echo esc_attr( wc_get_loop_prop( 'columns' ) ); ?>">
+<ul class="products columns-<?php echo esc_attr( wc_get_loop_prop( 'columns' ) ); ?>">
