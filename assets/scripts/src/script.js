cd155d9f54db1576d7c2345a9c2ffedd0d3350eb
Rick Butterfield <rick.butterfield@shoutdigital.com>
2018-04-16 11:26:51 +0100

    Update checkbox to support general sibling selector

--- assets/scripts/src/script.js
+++ assets/scripts/src/script.js
@@ -33,7 +33,7 @@ var Valley = (function() {
     /**
      * Scoped variables inside `Valley.#` for storing key bits of information
      */
-    Version: '3.4.1',
+    Version: '3.4.2',
 
     /**
      * Kick everything off
--- functions.php
+++ functions.php
@@ -4,7 +4,7 @@
  * Configs
  */
 
-define( 'VC_THEME_VERSION', '3.4.1' );
+define( 'VC_THEME_VERSION', '3.4.2' );
 define( 'CS_APP_ACCOUNT', 'valley' );
 define( 'CS_APP_APPLICATION', 'valleychurch-website' );
 define( 'CS_APP_AUTH', 'Dg8lHr5mIg30qcVdN7Je' );
