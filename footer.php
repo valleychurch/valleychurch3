7f4d932e14491a490cd0f4fdc0b4e12e7f119332
Rick Butterfield <rick.butterfield@shoutdigital.com>
2018-05-03 14:37:58 +0100

    Remove FullStory and add cookie policy link

--- footer.php
+++ footer.php
@@ -106,7 +106,7 @@
         <div class="row">
           <p class="small u-text-muted u-text-center@xs u-text-left@lg u-margin-none">
             &copy; <?= date('Y') ?> <?= bloginfo('name'); ?>. We're a <a href="http://freemethodist.org.uk" target="_blank">Free Methodist UK Church</a>, <a href="http://apps.charitycommission.gov.uk/Showcharity/RegisterOfCharities/CharityWithoutPartB.aspx?RegisteredCharityNumber=1125080&SubsidiaryNumber=0"
-              target="_blank">Registered Charity No. 1125080</a>. <a href="/privacy">Privacy &amp; Cookie Policy</a>.
+              target="_blank">Registered Charity No. 1125080</a>. <a href="/privacy">Privacy Policy</a>. <a href="/cookies">Cookie Policy</a>.
           </p>
         </div>
       </div>
@@ -119,23 +119,5 @@
   } ?>
 
     <?php wp_footer(); ?>
-
-    <script>
-    window['_fs_debug'] = false;
-    window['_fs_host'] = 'fullstory.com';
-    window['_fs_org'] = '9MYY5';
-    window['_fs_namespace'] = 'FS';
-    (function(m,n,e,t,l,o,g,y){
-        if (e in m) {if(m.console && m.console.log) { m.console.log('FullStory namespace conflict. Please set window["_fs_namespace"].');} return;}
-        g=m[e]=function(a,b){g.q?g.q.push([a,b]):g._api(a,b);};g.q=[];
-        o=n.createElement(t);o.async=1;o.src='https://'+_fs_host+'/s/fs.js';
-        y=n.getElementsByTagName(t)[0];y.parentNode.insertBefore(o,y);
-        g.identify=function(i,v){g(l,{uid:i});if(v)g(l,v)};g.setUserVars=function(v){g(l,v)};
-        g.identifyAccount=function(i,v){o='account';v=v||{};v.acctId=i;g(o,v)};
-        g.clearUserCookie=function(c,d,i){if(!c || document.cookie.match('fs_uid=[`;`]*`[`;`]*`[`;`]*`')){
-        d=n.domain;while(1){n.cookie='fs_uid=;domain='+d+
-        ';path=/;expires='+new Date(0).toUTCString();i=d.indexOf('.');if(i<0)break;d=d.slice(i+1)}}};
-    })(window,document,window['_fs_namespace'],'script','user');
-    </script>
   </body>
 </html>
