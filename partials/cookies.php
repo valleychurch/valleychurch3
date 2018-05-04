6586d4ecffd7838627bbb57bf2eea6d39228ef34
Rick Butterfield <rick.butterfield@shoutdigital.com>
2018-05-04 15:36:22 +0100

    Add space to cookie text

--- partials/cookies.php
+++ partials/cookies.php
@@ -3,7 +3,7 @@
   <div class="o-container o-container--full">
     <div class="o-flag o-flag--rev">
       <div class="o-flag__flex u-text-left">
-        <p class="u-text-left"><strong>This site uses cookies.</strong> Some of these cookies are essential, while others help us to improve your experience by providing insights into how the site is being used.<br class="u-hide u-show@md"/>Please view our <a href="/cookies">Cookie Policy</a> to find out more.</p>
+        <p class="u-text-left"><strong>This site uses cookies.</strong> Some of these cookies are essential, while others help us to improve your experience by providing insights into how the site is being used. <br class="u-hide u-show@md"/>Please view our <a href="/cookies">Cookie Policy</a> to find out more.</p>
       </div>
       <div class="o-flag__fix">
         <button aria-label="Dismiss notification" class="u-margin-none js-cookies-dismiss o-btn o-btn--ghost o-btn--ghost--white">
