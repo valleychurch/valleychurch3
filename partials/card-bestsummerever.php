4f206ad978bb3723affac37625f4ca9b4ba3fcbc
Rick Butterfield <rick.butterfield@shoutdigital.com>
2018-05-09 11:40:27 +0100

    Add BSE card styling

--- partials/card-bestsummerever.php
+++ partials/card-bestsummerever.php
@@ -1,5 +1,5 @@
 <article <?= post_class() ?> itemscope itemtype="http://schema.org/Event">
-  <div class="o-card u-text-center" title="<?= get_the_title(); ?>">
+  <div class="o-card o-card--bestsummerever u-text-center" title="<?= get_the_title(); ?>">
     <div class="o-card__body">
       <h2 class="h3 o-card__title" itemprop="name">
         <?php the_title(); ?>
