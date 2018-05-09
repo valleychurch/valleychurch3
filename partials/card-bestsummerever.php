8f9846257ed0d21469de5e76b46b8fabda155ae1
Rick Butterfield <rick.butterfield@shoutdigital.com>
2018-05-09 14:02:00 +0100

    Update card dates

--- partials/card-bestsummerever.php
+++ partials/card-bestsummerever.php
@@ -1,14 +1,26 @@
+<?php
+$datetimestart = strtotime( get_field('date_start') );
+$datetimeend = strtotime( get_field('date_end') );
+
+$location = get_field( 'event_location' );
+?>
+
 <article <?= post_class() ?> itemscope itemtype="http://schema.org/Event">
   <div class="o-card o-card--bestsummerever u-text-center" title="<?= get_the_title(); ?>">
     <div class="o-card__body">
       <h2 class="h3 o-card__title" itemprop="name">
         <?php the_title(); ?>
       </h2>
-      <?php if ( get_field( 'date_start' ) ) { ?>
       <h3 class="h4 o-card__subtitle u-margin-none u-text-black" <?= ( get_field( 'date_start' ) ? 'itemprop="startDate" content="' . date( 'c', get_field( 'date_start' ) ) . '"' : "" ); ?>>
-        <?= date('jS F', strtotime( get_field( 'date_start' ) ) ); ?>
+        <?php
+        if ( $datetimestart != $datetimeend ) {
+          echo date( 'jS', $datetimestart ) . "&ndash;" . date( 'jS M', $datetimeend );
+        }
+        else {
+          echo date( 'jS M', $datetimestart );
+        }
+        ?>
       </h3>
-      <?php } ?>
     </div>
   </div>
 </article>
